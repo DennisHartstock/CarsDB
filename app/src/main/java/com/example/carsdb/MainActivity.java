50c536f267e6526da92269f29510eee8e9e62b24
DennsHartstock <dennis.hartstock@web.de>
2022-01-11 05:37:43 +0100

    Logic for adding new cars to database

--- app/src/main/java/com/example/carsdb/MainActivity.java
+++ app/src/main/java/com/example/carsdb/MainActivity.java
@@ -3,12 +3,39 @@ package com.example.carsdb;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.example.carsdb.data.DatabaseHandler;
+import com.example.carsdb.model.Car;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+
+        DatabaseHandler databaseHandler = new DatabaseHandler(this);
+
+        databaseHandler.addCar(new Car("Toyota", "30 000 $"));
+        databaseHandler.addCar(new Car("Opel", "25 000 $"));
+        databaseHandler.addCar(new Car("Mercedes", "50 000 $"));
+        databaseHandler.addCar(new Car("Kia", "28 000 $"));
+        databaseHandler.addCar(new Car("Mazda", "30 000 $"));
+        databaseHandler.addCar(new Car("Honda", "25 000 $"));
+        databaseHandler.addCar(new Car("Skoda", "50 000 $"));
+        databaseHandler.addCar(new Car("Hyundai", "28 000 $"));
+
+        List<Car> carsList = databaseHandler.getAllCars();
+
+        for (Car car : carsList) {
+            Log.d("Car Info: ", "ID " + car.getId() + ", Name " + car.getName() + ", Price " + car.getPrice());
+        }
     }
 }
--- app/src/main/java/com/example/carsdb/data/DatabaseHandler.java
+++ app/src/main/java/com/example/carsdb/data/DatabaseHandler.java
@@ -1,12 +1,19 @@
 package com.example.carsdb.data;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
+import com.example.carsdb.model.Car;
 import com.example.carsdb.utils.Util;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class DatabaseHandler extends SQLiteOpenHelper {
 
@@ -28,5 +35,56 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
         sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + Util.TABLE_NAME);
         onCreate(sqLiteDatabase);
+    }
+
+    // CRUD
+    // Create, Read, Update, Delete
+
+    public void addCar(Car car) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(Util.KEY_NAME, car.getName());
+        contentValues.put(Util.KEY_PRICE, car.getPrice());
+
+        db.insert(Util.TABLE_NAME, null, contentValues);
+        db.close();
+    }
+
+    public Car getCar(int id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.query(Util.TABLE_NAME, new String[]{Util.KEY_ID, Util.KEY_NAME, Util.KEY_PRICE},
+                Util.KEY_ID + "=?", new String[]{String.valueOf(id)},
+                null, null, null, null);
+
+        if (cursor != null) {
+            cursor.moveToFirst();
+        }
+
+        Car car = new Car(Objects.requireNonNull(cursor).getInt(0), cursor.getString(1), cursor.getString(2));
+        return car;
+    }
+
+    public List<Car> getAllCars() {
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        List<Car> carsList = new ArrayList<>();
+
+        String selectAllCars = "SELECT * FROM " + Util.TABLE_NAME;
+        Cursor cursor = db.rawQuery(selectAllCars, null);
+
+        if (cursor.moveToFirst()) {
+
+            do {
+                Car car = new Car();
+                car.setId(cursor.getInt(0));
+                car.setName(cursor.getString(1));
+                car.setPrice(cursor.getString(2));
+                carsList.add(car);
+            } while (cursor.moveToNext());
+
+        }
+        return carsList;
     }
 }
